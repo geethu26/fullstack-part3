8d2168129ae950d0b6826be829819197c08c874a
Geethu Joseph <geethujoseph03@gmail.com>
2024-03-10 21:35:38 +0530

    Completing pqrt3

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,10 +45,10 @@ module.exports = {
         ],
         'eqeqeq':'error',
         'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
+        'object-curly-spacing': [
         'error', 'always'
-    ],
-    'arrow-spacing': [
+        ],
+        'arrow-spacing': [
         'error', { 'before': true, 'after': true }
     ]
     }
